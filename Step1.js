3b0474661ca0043022259755da7904eb1f40c94d
Jeff Lowery <jlowery2663@gmail.com>
2017-01-07 05:44:14 -0800

    Combinator

--- Step1.js
+++ Step1.js
@@ -1,48 +1,23 @@
 var _ = require('lodash');
+var Combinator = require('./combinator');
 
 var menu = {
-  iceCream: {min: 1, max: 2, values: ["CHOCOLATE", "STRAWBERRY", "VANILLA", "BUTTERSCOTCH"]},
+  iceCream: {min: 1, max: 2, values: ["CHOCOLATE", "STRAWBERRY", "VANILLA"]},
   topping: {min: 0, max: 2, values: ["pineapple", "strawberry", "coconut flakes", "pecans"]},
   syrup: {min:0, max: 1, values: ["chocolate", "marshmallow", "butterscotch", "maple"]}
 }
 
-/*
-makeSelection(itemKey, comboList, comboIndex)
-  select one (or none)
-  makeSelection(itemKey+1 , comboList + 1, comboIndex+1)
-  resolve
-*/
+var iceCreamChoices = new Combinator({min:menu.iceCream.min, max: menu.iceCream.max})
+    .combine([], menu.iceCream.values)
+    .combinations;
 
-var Combiner = function(opts) {
-  var combos = [];
-  function combo(active, rest) {
-      if (rest.length == 0) {
-        if (active.length >= (opts.min||0) &&
-            active.length <= (opts.max||active.length))
-          combos.push(active);
-      } else {
-          combo(active.concat(rest[0]), rest.slice(1, rest.length));
-          combo(active, rest.slice(1, rest.length));
-      }
-      return this;
-  }
-  return {
-    combos: combos,
-    combo: combo
-  }
-}
+var toppingChoices = new Combinator({min:menu.topping.min, max: menu.topping.max})
+    .combine([], menu.topping.values)
+    .combinations;
 
-var iceCreamChoices = new Combiner({min:menu.iceCream.min, max: menu.iceCream.max})
-    .combo([], menu.iceCream.values)
-    .combos;
-
-var toppingChoices = new Combiner({min:menu.topping.min, max: menu.topping.max})
-    .combo([], menu.topping.values)
-    .combos;
-
-var syrupChoices = new Combiner({min:menu.syrup.min, max: menu.syrup.max})
-    .combo([], menu.syrup.values)
-    .combos;
+var syrupChoices = new Combinator({min:menu.syrup.min, max: menu.syrup.max})
+    .combine([], menu.syrup.values)
+    .combinations;
 
  console.log(iceCreamChoices);
  console.log(toppingChoices);
